957d044d6a52794cae4e7e34635ca2e299628877
polsan22daw <polsan22daw@proton.me>
2022-10-30 21:43:30 +0100

    Inicialitzacio

---
+++ GP2-PedemonteSanzTorres/src/app/inici.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: 'inici.html',
+    styles: ['h1 { color: #900 }']
+})
+
+export class IniciComponent {
+
+}
