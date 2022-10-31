cb300ee0fa15d06b77c1581f86d39f05340097c0
polsan22daw <polsan22daw@proton.me>
2022-10-31 14:37:02 +0100

    menu inici v1

--- GP2-PedemonteSanzTorres/src/app/app.module.ts
+++ GP2-PedemonteSanzTorres/src/app/app.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatSliderModule } from '@angular/material/slider';
 import {MatSidenavModule} from '@angular/material/sidenav';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IniciComponent } from './inici';
@@ -16,7 +20,12 @@ import { IniciComponent } from './inici';
     BrowserModule,
     AppRoutingModule,
     MatSliderModule,
-    MatSidenavModule
+    MatSidenavModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule
   ],
   providers: [],
   bootstrap: [IniciComponent]
