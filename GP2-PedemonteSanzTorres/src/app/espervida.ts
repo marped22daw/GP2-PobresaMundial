f1820a5036642af783e56b8b8bba3d30a7e96045
polsan22daw <polsan22daw@proton.me>
2022-11-08 19:06:49 +0100

    taula espervida amb dades

--- GP2-PedemonteSanzTorres/src/app/espervida.ts
+++ GP2-PedemonteSanzTorres/src/app/espervida.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import EspervidaJson from './json/espervida.json';
 
 @Component({
     selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 
 export class EspervidaComponent {
+    //Poner Json en una tabla
+    displayedColumns: any[] = ['Pais', 'Any', 'Espervida', 'Adult_Mortality', 'Infant_Deaths'];
+    dataSource = EspervidaJson;
 }
