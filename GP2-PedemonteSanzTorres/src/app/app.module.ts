20c03ea14813c5994befd871f9698777cda93536
polsan22daw <polsan22daw@proton.me>
2022-11-10 19:08:51 +0100

    primer intent de grafic amb 3DJS

--- GP2-PedemonteSanzTorres/src/app/app.module.ts
+++ GP2-PedemonteSanzTorres/src/app/app.module.ts
@@ -21,6 +21,9 @@ import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { RouterLinkActive } from '@angular/router';
 import { RouterLink } from '@angular/router';
+import { BarComponent } from './bar/bar.component';
+import { PieComponent } from './pie/pie.component';
+import { ScatterComponent } from './scatter/scatter.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +31,10 @@ import { RouterLink } from '@angular/router';
   IniciComponent,
   EspervidaComponent,
   FertilitatComponent,
-  PobresaComponent
+  PobresaComponent,
+  BarComponent,
+  PieComponent,
+  ScatterComponent
 ],
   imports: [
     BrowserModule,
---
+++ GP2-PedemonteSanzTorres/src/app/bar/bar.component.ts
@@ -0,0 +1,84 @@
+import { Component, OnInit } from '@angular/core';
+import * as d3 from 'd3';
+import EspervidaJson from '../json/espervida.json';
+
+@Component({
+  selector: 'app-bar',
+  templateUrl: './bar.component.html',
+  styleUrls: ['./bar.component.css']
+})
+export class BarComponent implements OnInit {
+
+  private svg:any;
+  private margin = 50;
+  private width = 1350 - (this.margin * 2);
+  private height = 500 - (this.margin * 2);
+
+  private createSvg(): void {
+    this.svg = d3.select("figure#bar")
+    .append("svg")
+    .attr("width", this.width + (this.margin * 2))
+    .attr("height", this.height + (this.margin * 2))
+    .append("g")
+    .attr("transform", "translate(" + this.margin + "," + this.margin + ")");
+  }
+
+  private drawBars(data: any[]): void {
+    // Create the X-axis band scale
+    const x = d3.scaleBand()
+    .range([0, this.width])
+    .domain(data.map(d => d.EspervidaJson))
+    .padding(0.2);
+  
+    // Draw the X-axis on the DOM
+    this.svg.append("g")
+    .attr("transform", "translate(0," + this.height + ")")
+    .call(d3.axisBottom(x))
+    .selectAll("text")
+    .attr("transform", "translate(-10,0)rotate(-45)")
+    .style("text-anchor", "end");
+  
+    // Create the Y-axis band scale
+    const y = d3.scaleLinear()
+    .domain([0, 100])
+    .range([this.height, 0]);
+  
+    // Draw the Y-axis on the DOM
+    this.svg.append("g")
+    .call(d3.axisLeft(y));
+  
+    // Create and fill the bars
+    this.svg.selectAll("bars")
+    .data(data)
+    .enter()
+    .append("rect")
+    .attr("x", d => x(d.EspervidaJson))
+    .attr("y", d => y(d.Stars))
+    .attr("width", x.bandwidth())
+    .attr("height", (d) => this.height - y(d.Stars))
+    .attr("fill", "#d04a35");
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.createSvg();
+    //this.drawBars(this.data);
+    this.drawBars(EspervidaJson
+      .map(d => {
+        return {
+          EspervidaJson: d.Any,
+          Stars: d.Espervida
+        };
+        }));
+
+      // {
+      //   Pais: String;
+      //   Any: Number;
+      //   Espervida: Number;
+      //   Adult_Mortality: Number;
+      //   Infant_Deaths: Number;
+      // }
+          
+  }
+}
