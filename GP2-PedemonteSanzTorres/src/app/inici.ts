1a9e9496c36ae22bc2fa36d7ce19fb78608b40c9
marped22daw <marped22daw@proton.me>
2022-11-19 16:08:48 +0100

    titulo animacion flyout

--- GP2-PedemonteSanzTorres/src/app/inici.ts
+++ GP2-PedemonteSanzTorres/src/app/inici.ts
@@ -2,21 +2,46 @@ import { Component, ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
 import {BreakpointObserver} from '@angular/cdk/layout';
 import { Router } from '@angular/router';
-
+import { Input } from '@angular/core';
+import { HostBinding } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  // ...
+} from '@angular/animations';
 @Component({
     selector: 'app-root',
     templateUrl: './inici.html',
-    styleUrls: ['./inici.css']
+    styleUrls: ['./inici.css'],
+    animations: [
+        // animation triggers go here
+        // trigger('changeState', [
+        //     state ('stateLogin', style({transform: 'translateX(-50%)', opacity: 1})),
+        //     state ('stateRegister', style({transform: 'translateX(150%)', opacity: 0, display: 'none'})),
+        //     transition('stateRegister=>stateLogin', [animate('0.3s ease-in')]),
+        //     transition('stateLogin=>stateRegister', [animate('0.3s ease-out')])
+        // ])
+        trigger('flyInOut', [
+            state('in', style({ transform: 'translateY(0)' })),
+            transition('void => *', [
+              style({ transform: 'translateY(-100%)' }),
+              animate(300)
+            ])
+          ])
+      ]
 })
 
 export class IniciComponent {
+    @Input() currentState;
     @ViewChild(MatSidenav) 
     sidenav!: MatSidenav;
     
     constructor(private observer: BreakpointObserver, public router: Router) {
 
     }
-
     ngAfterViewInit() {
         this.observer.observe(['']).subscribe(result => {
             if (result.matches) {
