6cabfc7f5e3d039a4ce102b655743b651c31b1c4
angtor22daw <angtor22daw@proton.me>
2022-11-19 12:00:18 +0100

    pagina inicial v1

--- GP2-PedemonteSanzTorres/src/app/pipes/Pipe_punts.ts
+++ GP2-PedemonteSanzTorres/src/app/pipes/Pipe_punts.ts
@@ -1,13 +1,13 @@
 import { Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({
-    name: "punts"
-  })
-  export class PuntsPipe implements PipeTransform {
-    transform(value: any, args?: any): any {
-        if (value) {
-            return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-        }
-        return value;
-        }
+  name: "punts"
+})
+export class PuntsPipe implements PipeTransform {
+  transform(value: any, args?: any): any {
+    if (value) {
+      return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+    }
+    return value;
   }
+}
